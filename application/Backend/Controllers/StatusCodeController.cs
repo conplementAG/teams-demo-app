e0526fa682ed81eafe6e85134261ab3528669667
Peter Ittner <peter.ittner.cp@hotmail.com>
2019-07-26 11:17:24 +0200

    feat: adds status code controller to simulate 500 responses

---
+++ application/Backend/Controllers/StatusCodeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusCodeController : ControllerBase
+    {
+        /// <summary>
+        /// Genreates an empty response with the given status code.
+        /// Could be used to monitor the response codes with Prometheus.
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        [HttpGet("{code}")]
+        public ActionResult GenerateHttpStatusCodeResponse(int code)
+        {
+            if (code <= 0)
+            {
+                return BadRequest();
+            }
+
+            return StatusCode(code);
+        }
+    }
+}
