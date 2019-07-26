3b1ed14db623ad922ebb2fdc301e6e480851ee6f
Peter Ittner <peter.ittner.cp@hotmail.com>
2019-07-26 11:17:05 +0200

    feat: adds memory load functionality (does not work properly on local docker)

--- application/Backend/Controllers/LoadController.cs
+++ application/Backend/Controllers/LoadController.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Backend.Load;
-using Backend.Model;
-using Backend.Repository;
+﻿using Backend.Load;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
@@ -15,21 +7,39 @@ namespace Backend.Controllers
     [ApiController]
     public class LoadController : ControllerBase
     {
+        /// <summary>
+        /// Generates CPU load
+        /// </summary>
+        /// <param name="percentage"></param>
+        /// <param name="durationInSeconds"></param>
+        /// <returns></returns>
         [HttpPost("cpu")]
         public ActionResult GenerateCPULoad(int percentage, int durationInSeconds)
         {
-            if (percentage < 0 || percentage > 100)
+            if (percentage < 0 || percentage > 100 || durationInSeconds <= 0)
             {
-                throw new ArgumentException("percentage");
-            }
-
-            if(durationInSeconds <= 0)
-            {
-                throw new ArgumentException("durationInSeconds");
+                return BadRequest();
             }
 
             new CPULoadGenerator().GenerateLoad(percentage, durationInSeconds);
+            return Ok();
+        }
 
+        /// <summary>
+        /// Allocates the given amount of memory. Garbage collection is only performed when needed (extreme low memory).
+        /// </summary>
+        /// <param name="megabytes"></param>
+        /// <param name="durationInSeconds"></param>
+        /// <returns></returns>
+        [HttpPost("memory")]
+        public ActionResult GenerateMemoryLoad(int megabytes, int durationInSeconds)
+        {
+            if(megabytes <= 0 || durationInSeconds <= 0)
+            {
+                return BadRequest();
+            }
+
+            new MemoryLoadGenerator().GenerateLoad(megabytes, durationInSeconds);
             return Ok();
         }
     }
